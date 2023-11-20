00b19f718ceb1bb088da253657068f1ab7909404
gitesh <124420820+gitesh-singla@users.noreply.github.com>
2023-11-20 14:23:33 +0530

    Fix spots showing at invalid times

--- server/functions/checkAvailability.js
+++ server/functions/checkAvailability.js
@@ -1,6 +1,7 @@
 const Bookings = require("../models/Bookings");
 
 async function checkAvailability(spot, slots, start, end){
+    if(start >= end) return false;
     const booked = await Bookings.find({
         spot: spot,
         start: {$lt: end },
