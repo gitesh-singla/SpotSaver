76ea4217fe7ca0593d98e012c13e44ddb31e2064
gitesh <124420820+gitesh-singla@users.noreply.github.com>
2024-03-13 22:25:20 +0530

    add validation to mongoose schemas

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-    name: {type: String, required: true},
-    email: {type: String, required: true,  unique: true},
-    phone: {type: String, required: true},
-    password: {type: String, required: true},
+    name: { type: String, match: /[a-zA-Z]/, required: true },
+    email: { type: String, match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, required: true, unique: true },
+    phone: { type: String, match: /^\d+$/, required: true },
+    password: { type: String, minLength: 8, required: true },
 });
 
 const UserModel = mongoose.model('users', UserSchema);
